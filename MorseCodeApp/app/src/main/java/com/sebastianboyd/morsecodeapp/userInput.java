92fc325ebb325f9d6ef8ecbed91aa8e9dc11ab45
SebastianBoyd <Family@chrisbosmacbook.att.net>
2014-11-08 18:10:08 -0800

    Added split

--- MorseCodeApp/app/src/main/java/com/sebastianboyd/morsecodeapp/userInput.java
+++ MorseCodeApp/app/src/main/java/com/sebastianboyd/morsecodeapp/userInput.java
@@ -1,5 +1,6 @@
 package com.sebastianboyd.morsecodeapp;
 import android.widget.*;
+import android.util.Log;
 
 /**
  * Created by Duncan Grubbs on 11/8/14.
@@ -8,6 +9,5 @@ public class userInput extends MyActivity{
 
 
     EditText userInput = (EditText)findViewById(R.id.userInput);
-    String[] input = new String[1];
-
+    String[] input = userInput.split("");
 }
