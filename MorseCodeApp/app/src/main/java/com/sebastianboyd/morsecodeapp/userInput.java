5c67ea65a80cf319d5e1f66b754e3158a04dcac0
duncangrubbs <duncan.grubbs@gmail.com>
2014-11-08 17:36:42 -0800

    Changes to User input and GUI

--- MorseCodeApp/app/src/main/java/com/sebastianboyd/morsecodeapp/userInput.java
+++ MorseCodeApp/app/src/main/java/com/sebastianboyd/morsecodeapp/userInput.java
@@ -1,9 +1,15 @@
 package com.sebastianboyd.morsecodeapp;
+import android.widget.*;
 
 /**
  * Created by Duncan Grubbs on 11/8/14.
  */
-public class userInput {
+public class userInput extends MyActivity{
+
+
+    EditText userInput = (EditText)findViewById(R.id.userInput);
+
+
 
 
 }
